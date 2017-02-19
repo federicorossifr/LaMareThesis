6a01f6b720e7e34c93e57e6b1a80c9fe46cb984e
Federico Rossi <f.rossi57@studenti.unipi.it>
2017-02-19 20:22:03 +0100

    Initial commit
    First commit.

---
+++ clean.bat
@@ -0,0 +1,11 @@
+del *.log
+REM del *.dvi
+del *.aux
+del *.bbl
+del *.blg
+REM del *.brf
+del *.out
+del *.toc
+del main.run.xml
+del main-blx.bib
+del BIBOUT
---
+++ compile.bat
@@ -0,0 +1,15 @@
+@echo off
+cls
+echo PDFLATEX --- 1/5
+pdflatex -quiet main.tex 
+echo BIBTEX --- 2/5
+bibtex -quiet main >> BIBOUT
+echo PDFLATEX --- 3/5
+pdflatex -quiet main.tex
+echo PDFLATEX --- 4/5
+pdflatex -quiet main.tex
+echo PDFLATEX --- 5/5
+pdflatex -quiet main.tex
+echo DONE --- OPENING PDF 1/1
+START "" main.pdf
+clean.bat
